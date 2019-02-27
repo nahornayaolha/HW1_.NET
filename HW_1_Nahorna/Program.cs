b5214fd29ed9e65db2498a72e1421a09346e3a82
nahornayaolha <nahornayaolha@users.noreply.github.com>
2019-02-27 20:47:31 +0200

    «stage»

---
+++ HW_1_Nahorna/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_1_Nahorna
+{
+    class Program
+    {
+        static int[] arr = new int[5];
+
+        static void PutMethod()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        static void InputMethod()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                arr[i] *= 10;
+                Console.Write(arr[i]+" ");
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            PutMethod();
+            Console.WriteLine("User input finished. Processing...");
+            InputMethod();
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
